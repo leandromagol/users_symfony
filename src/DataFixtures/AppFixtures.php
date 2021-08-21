e82611030608dcce6522ac87055be62c3b4d2fad
Leandro Oliveira <36287525+leandromagol@users.noreply.github.com>
2021-08-21 13:25:54 -0300

    Add fixtures and soft deletable user

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use App\Helpers\Factories\UserFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    private $userFactory;
+    public function __construct(UserFactory $userFactory)
+    {
+     $this->userFactory = $userFactory;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = $this->userFactory->BuildUser([
+            'email'=>'admin@admin.com',
+                'password'=>12345678
+        ]);
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
